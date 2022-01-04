980cef0aaed5d8eeee13c0e0152a0359d50eef92
EnriqueIzel2 <eleaoizel@gmail.com>
2022-01-04 13:19:03 -0400

    First updare

---
+++ src/src/pages/Repo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Repo() {
+  return (
+    <div>
+      <h1>Repo</h1>
+    </div>
+  );
+}
+
+export default Repo;
---
+++ src/src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Repo from "./pages/Repo";
+
+export default function HandleRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/repo/:repo" element={<Repo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
