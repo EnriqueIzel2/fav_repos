ed4ec06d3e11eac46ba77caa1e5136c0b7016900
EnriqueIzel2 <eleaoizel@gmail.com>
2022-01-05 10:27:15 -0400

    feat: create title and form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { FaGithub, FaPlus } from "react-icons/fa";
 
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
 
 function Home() {
   return (
-    <div>
-      <Title>Home</Title>
-    </div>
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
