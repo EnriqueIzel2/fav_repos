218db1ceac7190660ce54df2da68e1fffdee0c01
EnriqueIzel2 <eleaoizel@gmail.com>
2022-01-05 11:11:47 -0400

    feat: create handleSubmit function
    handleSubmit handle when the user search for a new repo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { FaGithub, FaPlus } from "react-icons/fa";
+
+import api from "../../services/api";
 
 import { Container, Form, SubmitButton } from "./styles";
 
 function Home() {
   const [newRepo, setNewRepo] = useState("");
+  const [repos, setRepos] = useState([]);
 
   function handleInputChange(e) {
     setNewRepo(e.target.value);
   }
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      async function submit() {
+        const response = await api.get(`repos/${newRepo}`);
+
+        const data = {
+          name: response.data.full_name,
+        };
+
+        setRepos([...repos, data]);
+        setNewRepo("");
+      }
+
+      submit();
+    },
+    [newRepo, repos]
+  );
 
   return (
     <Container>
@@ -17,7 +40,7 @@ function Home() {
         Meus repositórios
       </h1>
 
-      <Form onSubmit={() => {}}>
+      <Form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Adicionar repositório"
