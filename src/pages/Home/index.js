e0dac930d86ef9324eeb635d56e492aef262de8d
EnriqueIzel2 <eleaoizel@gmail.com>
2022-01-10 19:31:45 -0400

    feat: create navigation to repo page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import api from "../../services/api";
 
@@ -109,9 +110,9 @@ function Home() {
               </DeleteButton>
               {repo.name}
             </span>
-            <a href="#">
+            <Link to={`/repos/${encodeURIComponent(repo.name)}`}>
               <FaBars size={20} />
-            </a>
+            </Link>
           </li>
         ))}
       </List>
